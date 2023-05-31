8a406e7f1bc87d7f33124d04565f99dcdf2f2033
elinordeniz <elifnurdenizmd@gmail.com>
2023-05-31 22:33:40 +0300

    serverless added

---
+++ functions/app.js
@@ -0,0 +1,55 @@
+require("dotenv").config();
+const serverless = require("serverless-http");
+const cors = require("cors");
+const bodyParser= require('body-parser')
+const express = require("express");
+const app = express();
+const router = express.Router();
+const fs = require("fs")
+const PORT = process.env.PORT || 1299;
+const wordRouter = require("../routes/word");
+const notFoundMiddleware = require("../middlewares/not-found");
+const customErrorHandlerMiddleware = require("../middlewares/error-handler");
+const swaggerUI= require('swagger-ui-express');
+const YAML= require('yamljs');
+
+var path = require('path');
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json({ strict: false }));
+
+let swagger_path = path.resolve(__dirname,'../swagger.yaml');
+ console.log(swagger_path)
+const file  = fs.readFileSync(swagger_path, 'utf8')
+ console.log(file)
+const swaggerDocument= YAML.parse(file)
+//const swaggerDocument = require('../swagger.json');
+
+var options = {
+   
+     "isTrusted": true 
+  
+};
+//routes
+app.use("/.netlify/functions/app", router);
+app.use("/", router);
+
+router.get("/", (req, res) => {
+  res.send('<h1>Welcome to Mixy Mixy Word Game Api</h1><a href="/swagger">Documentation</a>');
+});
+
+// router.use("/swagger", swaggerUI.serve);
+// router.get("/swagger", swaggerUI.setup(swaggerDocument, options));
+
+router.use("/swagger", cors(), swaggerUI.serve, swaggerUI.setup(swaggerDocument, options));
+router.use("/api/v1/word", wordRouter);
+
+// error middlewares
+router.use(notFoundMiddleware);
+router.use(customErrorHandlerMiddleware);
+
+// app.listen(PORT, () => {
+//   console.log(`Server is listenin on PORT ${PORT}`);
+// });
+
+module.exports.handler = serverless(app);
